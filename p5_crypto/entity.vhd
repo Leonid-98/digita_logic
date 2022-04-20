c983f662d3cf82a5b652947dc5737076be52f03e
Leonid-98 <75499802+Leonid-98@users.noreply.github.com>
2022-04-20 11:09:23 +0300

    praks5 done

--- p5_crypto/entity.vhd
+++ p5_crypto/entity.vhd
@@ -5,11 +5,11 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 
 entity crypto is
---    generic (
---        ror_val: integer := 2;
---        rol_val: integer := 1;
---        key_len: integer := 8
---    );
+    generic (
+        ror_val: integer := 2;
+        rol_val: integer := 1;
+        key_len: integer := 8
+    );
     port (clk : in std_logic;
 
          data_in : in std_logic_vector(6 downto 0); 
@@ -27,8 +27,8 @@ architecture Behavioral of crypto is
     signal clkdiv  : std_logic_vector(24 downto 0);
     signal cclk    : std_logic;
     
-    signal data_and_parity: std_logic_vector(7 downto 0);
-    
+--    signal signal_crypt: std_logic_vector(7 downto 0);
+--    signal signal_decrypt: std_logic_vector(7 downto 0);
 begin
     
     slow_clk: process (clk)
@@ -39,15 +39,64 @@ begin
     end process slow_clk;
     cclk <= clkdiv(24);
     
-    parity_calculator: process(cclk)
+    main: process(cclk)
+    variable count : unsigned(6 downto 0) := "0000000";
+    variable parity_val : std_logic_vector(6 downto 0);
+    
+    variable data_and_parity: std_logic_vector(7 downto 0);
     begin
-        data_and_parity(7) <= data_in(0);
-        data_and_parity(6 downto 0) <= data_in;
+        -- parity count
+        count := "0000000";  
+        for i in 0 to 6 loop   
+            if(data_in(i) = '1') then
+                count := count + 1;
+            end if;
+        end loop;
         
---        data_and_parity <= data_and_parity((ror_val - 1) downto 0) & data_and_parity(7 downto ror_val); 
-    end process parity_calculator;
+        parity_val := std_logic_vector(count);
+        data_and_parity(7) := parity_val(0);
+        data_and_parity(6 downto 0) := data_in;
+        
+        --xnor key
+        data_and_parity := data_and_parity xnor key;
+        
+        -- shift right
+        for  i in 1 to ror_val loop
+            data_and_parity := data_and_parity(0) & data_and_parity(7 downto 1);
+        end loop ;
+        
+        -- shift left
+        for  i in 1 to rol_val loop
+            data_and_parity := data_and_parity(6 downto 0) & data_and_parity(7);
+        end loop ;
+        
+        ----------------------------------------------------------
+        data_out_crypt <= data_and_parity;
+        ----------------------------------------------------------
+        
+        
+        -- DECRYPT
+        -- shift right back
+        for  i in 1 to rol_val loop
+            data_and_parity := data_and_parity(0) & data_and_parity(7 downto 1);
+        end loop ;
+        
+        -- shift left back
+        for  i in 1 to ror_val loop
+            data_and_parity := data_and_parity(6 downto 0) & data_and_parity(7);
+        end loop;
+        
+        --xnor key
+        data_and_parity := data_and_parity xnor key;
+        
+
+        ----------------------------------------------------------
+        data_out_decryp <= data_and_parity;
+        ----------------------------------------------------------
+        
+    end process main;
     
+--    data_out_decryp <= signal_decrypt;
+--    data_out_crypt <= signal_crypt;
     
-    
-    data_out_decryp <= data_and_parity;
 end Behavioral;
