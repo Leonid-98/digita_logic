be52d755795cbfcddeb327dcfc2680d7b22ebd0d
Leonid-98 <75499802+Leonid-98@users.noreply.github.com>
2022-04-29 17:31:54 +0300

    Update vga2.vhd

--- p6_vga/vga2.vhd
+++ p6_vga/vga2.vhd
@@ -3,10 +3,14 @@ use IEEE.STD_LOGIC_1164.ALL;
 USE ieee.NUMERIC_STD.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
+-- https://forum.digikey.com/t/vga-controller-vhdl/12794
+-- http://www.ece.ualberta.ca/~elliott/ee552/studentAppNotes/1998_w/Altera_UP1_Board_Map/vga.html
+-- https://digilent.com/reference/_media/basys3:basys3_rm.pdf
+
 entity vga is
     Port (
-        -- VGA
         clk: in std_logic;
+
         RED, GRN, BLU : out std_logic_vector(3 downto 0);
         HSYNC: out std_logic;
         VSYNC: out std_logic
@@ -14,14 +18,8 @@ entity vga is
 end vga;
 
 architecture Behavioral of vga is
-    constant img_width: integer := 256;
-    constant img_height: integer := 256;
-    constant img_size: integer := img_height * img_width;
-
-    -- VGA signals (eelmine praks) --------------------------------------------------------
     constant width: integer := 640;
     constant height: integer := 480;
-
     -- 25MHz pixel clock and 60Hz vertical refresh
     constant v_pulse_width: integer := 2;
     constant v_front_porch: integer := 10;
@@ -33,6 +31,7 @@ architecture Behavioral of vga is
 
     signal pixel_x: integer := 0;
     signal pixel_y: integer := 0;
+    signal pixel_count: integer := 0;
 
     constant display_x: integer := width + h_front_porch + h_pulse_width + h_back_porch;
     constant display_y: integer := height + v_front_porch + v_pulse_width + v_back_porch;
@@ -40,30 +39,33 @@ architecture Behavioral of vga is
     signal clk_div: std_logic_vector(1 downto 0) := "00";
     signal clk_25: std_logic;  -- slow clock 25 MHz
 
+    -- BRAM constants
+    constant img_width: integer := 256;
+    constant img_height: integer := 256;
+    constant img_size: integer := img_height * img_width;
+
+    signal clka : STD_LOGIC;
+    signal wea : STD_LOGIC_VECTOR(0 DOWNTO 0):= "0";
+    signal addra : STD_LOGIC_VECTOR(15 DOWNTO 0):=(others=>'0');
+    signal dina : STD_LOGIC_VECTOR(11 DOWNTO 0):=(others=>'0');
+    signal douta : STD_LOGIC_VECTOR(11 DOWNTO 0):=(others=>'0');
 
     component vga_memory PORT(
             clka : IN STD_LOGIC;
-            wea : IN STD_LOGIC;
+            wea : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
             addra : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
             dina : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
             douta : OUT STD_LOGIC_VECTOR(11 DOWNTO 0)
-
-        ); end component;
-    -- BRAM signals
-    signal sig_clka : STD_LOGIC := '0';
-    signal sig_wea : STD_LOGIC := '0';
-    signal sig_addra : STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
-    signal sig_dina : STD_LOGIC_VECTOR(11 DOWNTO 0) := "000000000000";
-    signal sig_douta : STD_LOGIC_VECTOR(11 DOWNTO 0) := "000000000000";
-
+        );
+    end component;
 
 begin
-    test1: vga_memory PORT MAP (
-            clka => sig_clka,
-            wea => sig_wea,
-            addra => sig_addra,
-            dina => sig_dina,
-            douta => sig_douta
+    port_map_vga : vga_memory PORT MAP(
+            clka => clka,
+            wea => wea,
+            addra => addra,
+            dina => dina,
+            douta => douta
         );
 
     clk_25MHz: process (clk)
@@ -72,6 +74,7 @@ begin
             clk_div <= clk_div + 1;
         end if;
         clk_25 <= clk_div(1);
+        clka <= clk_25;
     end process clk_25MHz;
 
     pixel_move: process (clk_25)
@@ -79,6 +82,7 @@ begin
         if rising_edge(clk_25) then
             if pixel_x = (display_x - 1) then
                 pixel_x <= 0;
+                pixel_count <= 0;
                 ----
                 if pixel_y = (display_y - 1) then
                     pixel_y <= 0;
@@ -88,6 +92,7 @@ begin
             ----
             else
                 pixel_x <= pixel_x + 1;
+                pixel_count <= pixel_count + 1;
 
             end if;
         end if;
@@ -119,15 +124,31 @@ begin
     show_color: process (clk_25)
     begin
         if rising_edge(clk_25) then
-            if (unsigned(sig_addra) < img_size) then
-                RED <= sig_douta(11 downto 8);
-                GRN <= sig_douta(7 downto 4);
-                BLU <= sig_douta(3 downto 0);
-                sig_addra <= STD_LOGIC_VECTOR(unsigned(sig_addra)+1);
+            if pixel_x < width and pixel_y < height then
+            
+                if pixel_x < img_width and pixel_y < img_height then
+                
+                    RED <= douta(11 downto 8);
+                    GRN <= douta(7 downto 4);
+                    BLU <= douta(3 downto 0);
+                    addra <= STD_LOGIC_VECTOR(unsigned(addra) + 1);
+                    
+                    if pixel_count >= img_size then
+                        addra <= (others => '0');
+                        pixel_count <= 0;
+                    end if;
+                    
+                else
+                    RED <= (others => '1');
+                    GRN <= (others => '0');
+                    BLU <= (others => '0');
+                end if;
             else
-                RED<=(others=>'0');
-                GRN<=(others=>'0');
-                BLU<=(others=>'0');
+                addra <= (others => '0');
+                RED <= "0000";
+                GRN <= "0000";
+                BLU <= "0000";
+
             end if;
         end if;
     end process show_color;
